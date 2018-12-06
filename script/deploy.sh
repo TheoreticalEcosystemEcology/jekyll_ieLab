5714552a37f2ced280cf4158a0b01eb0c6bb462d
Steve Vissault <s.vissault@yahoo.fr>
2018-12-06 00:07:10 -0500

    deploy procedure

---
+++ script/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+abort() {
+    local message=$1
+    echo $message
+    exit -1
+}
+[ -z $FTP_PASS ] && abort "FTP_PASS is undefined"
+[ -z $FTP_USER ] && abort "FTP_USER is undefined"
+[ -z $FTP_SITE ] && abort "FTP_SITE is undefined"
+lftp -u $FTP_USER,$FTP_PASS $FTP_SITE \
+ -e 'mirror -c -e -R _site ~ ; exit'
