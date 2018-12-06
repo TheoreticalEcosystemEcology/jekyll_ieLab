0d64fda9427b082c02eaab419b652962782371ea
Steve Vissault <s.vissault@yahoo.fr>
2018-12-06 08:49:34 -0500

    Set debug on

--- script/deploy.sh
+++ script/deploy.sh
@@ -7,5 +7,5 @@ abort() {
 [ -z $FTP_PASS ] && abort "FTP_PASS is undefined"
 [ -z $FTP_USER ] && abort "FTP_USER is undefined"
 [ -z $FTP_SITE ] && abort "FTP_SITE is undefined"
-lftp -u $FTP_USER,$FTP_PASS $FTP_SITE \
- -e 'set ssl:verify-certificate no; set debug on; mirror --ignore-time --parallel=10 -e -v -R _site ~ ; exit'
+lftp -d -u $FTP_USER,$FTP_PASS $FTP_SITE \
+ -e 'set ssl:verify-certificate no; mirror --ignore-time --parallel=10 -e -v -R _site ~ ; exit'
